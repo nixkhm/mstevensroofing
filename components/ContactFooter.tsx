6a90dac369d0e054903b998a5ae36fdcc7279e9e
Nicholas Masters <nicholas.masters20@stjohns.edu>
2023-12-30 18:37:14 -0500

    footer fixes

--- components/ContactFooter.tsx
+++ components/ContactFooter.tsx
@@ -3,61 +3,78 @@ import waypoint from '@/public/footer/waypoint.png'
 import email from '@/public/footer/email.png'
 
 import Image from 'next/image'
+import Button from './Button'
+import { twMerge } from 'tailwind-merge'
 
 const ContactFooter = () => {
   return (
     <div className='bg-yellow-400 text-black relative'>
-      <div className='flex lg:flex-row flex-col justify-evenly items-center text-center p-4'>
+      <div className='flex lg:flex-row flex-col justify-evenly items-center p-4'>
         <div className='flex-col relative'>
-          <h1 className='text-2xl font-extrabold'>Call us now!</h1>
-          <div className='flex items-center mb-10'>
+          <h1 className='text-2xl font-extrabold lg:mb-2'>Call us now!</h1>
+          <div className='flex items-center lg:mb-10 mb-5'>
             <Image
               src={phone}
               alt='phone'
               width={50}
               height={50}
-              className='opacity-70 lg:block hidden mr-2'
+              className='opacity-70 lg:block hidden mr-2 hover:scale-125 transition'
             />
             <div className='flex flex-col ml-4'>
               <h2 className='text-lg font-semibold'>Office: 631-345-2539</h2>
-              <h2 className='text-lg font-semibold'>Mobile: 631-662-1862</h2>
+              <h2 className='text-lg font-semibold'>
+                Mobile (SMS): 631-662-1862
+              </h2>
             </div>
           </div>
         </div>
         <div className='flex-col relative'>
-          <h1 className='text-2xl font-extrabold'>Our Locations</h1>
-          <div className='flex items-center mb-10'>
+          <h1 className='text-2xl font-extrabold lg:mb-2 lg:block hidden mb-6'>
+            Our Locations
+          </h1>
+          <div className='flex items-center lg:mb-10 mb-5'>
             <Image
               src={waypoint}
               alt='phone'
               width={50}
               height={80}
-              className='opacity-70 lg:block hidden mr-2'
+              className='opacity-70 lg:block hidden hover:scale-125 transition'
             />
-            <div className='flex flex-col ml-4'>
-              <h2 className='text-lg font-semibold'>
-                29 Lincoln Rd, Medford, NY 11763
+            <div className='flex flex-row text-center'>
+              <h2 className='text-lg font-semibold p-2'>
+                29 Lincoln Rd
+                <br />
+                Medford, NY 11763
               </h2>
-              <h2 className='text-lg font-semibold'>
-                53 Hill St, Southampton, NY 11968
+              <h2 className='text-lg font-semibold p-2'>
+                53 Hill St
+                <br />
+                Southampton, NY 11968
               </h2>
             </div>
           </div>
         </div>
         <div className='flex-col relative'>
-          <h1 className='text-2xl font-extrabold'>Send a Message</h1>
-          <div className='flex items-center mb-10'>
+          <h1 className='text-2xl font-extrabold mb-2'>Send a Message</h1>
+          <div className='flex items-center'>
             <Image
               src={email}
               alt='phone'
-              width={100}
-              height={50}
-              className='opacity-70 lg:block hidden mr-2'
+              width={79}
+              height={80}
+              className='opacity-70 lg:block hidden mr-2 mb-6 hover:scale-125 transition'
             />
-            <div className='flex flex-col ml-4'>
+            <div className='flex flex-col'>
               <h2 className='text-lg font-semibold'>
                 mstevensroofing@optonline.net
               </h2>
+              <div className='flex items-center justify-center'>
+                <Button
+                  className={twMerge('p-2 mt-2 border-black border-4 text-lg')}
+                >
+                  Contact
+                </Button>
+              </div>
             </div>
           </div>
         </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,14 +11,14 @@ const Footer = () => {
       <ContactFooter />
       <div className='flex bg-black text-white text-xl items-center justify-center text-center p-8'>
         {/* Left Section: Logo and Social Links */}
-        <div className='flex flex-row mr-16'>
+        <div className='flex flex-row items-center mr-14'>
           <div className='items-center justify-center align-center'>
             <Image
               src={logowht}
               alt='Logo'
               className='hover:scale-110 transition'
             />
-            <div className='flex flex-row mt-4 ml-15'>
+            <div className='flex flex-row mt-4 ml-16 items-center'>
               <SocialIcon
                 url='https://www.facebook.com/mstevensroofing'
                 target='_blank'
@@ -42,6 +42,18 @@ const Footer = () => {
                 }}
                 bgColor='transparent'
                 className='bg-red-600 hover:text-white rounded-full hover:bg-gray-700'
+              />
+              <SocialIcon
+                url='https://www.google.com/localservices/prolist?g2lbs=ANTchaMqfWXLztNu0KIPi0dJ4uE9AbVh0p28AwdUL_Eqb4mRfTyhynNkYYlEiPlxYKjdmPLpJFSBFinFL_letbz_XgFV72TvBP8b_oCw2PkMRSLrPJHhhNg%3D&hl=en-US&gl=us&ssta=1&q=m%20stevens%20roofing&oq=m%20stevens%20roofing&slp=MgA6HENoTUluZTI4bEtpNGd3TVZOcGVEQngycDFBVFhSAggCYAB6-AFDaEZ0SUhOMFpYWmxibk1nY205dlptbHVaMGlUdXFqVDVZQ0FnQWhhSHhBQUVBRVFBaGdBR0FFWUFpSVJiU0J6ZEdWMlpXNXpJSEp2YjJacGJtZDZEbE4xWm1admJHc2dRMjkxYm5SNWtnRVNjbTl2Wm1sdVoxOWpiMjUwY21GamRHOXlxZ0ZSRUFFcUZTSVJiU0J6ZEdWMlpXNXpJSEp2YjJacGJtY29BRElmRUFFaUczR25Xb1lGRnJKYWFxUTNQdDExd0F1cTljVGlDV2VjVG9OUWJESVZFQUlpRVcwZ2MzUmxkbVZ1Y3lCeWIyOW1hVzVuNEFFQZIBIgoNL2cvMTFqbXozbmRqYgoLL2cvMXRmNm43OG0SBAoCCAE%3D&src=2&spp=CgsvZy8xdGY2bjc4bTq8AVdoOFFBQkFCRUFJWUFCZ0JHQUlpRVcwZ2MzUmxkbVZ1Y3lCeWIyOW1hVzVua2dFU2NtOXZabWx1WjE5amIyNTBjbUZqZEc5eW1nRUFxZ0ZSRUFFcUZTSVJiU0J6ZEdWMlpXNXpJSEp2YjJacGJtY29BRElmRUFFaUczR25Xb1lGRnJKYWFxUTNQdDExd0F1cTljVGlDV2VjVG9OUWJESVZFQUlpRVcwZ2MzUmxkbVZ1Y3lCeWIyOW1hVzVu&serdesk=1&lrlstt=1703978932309&ved=2ahUKEwjVs6-UqLiDAxXhGFkFHR80C-0QvS56BAgWEAE&scp=ChdnY2lkOnJvb2ZpbmdfY29udHJhY3RvchI-EhIJ1_mbyICu4IkRU18dDUkMxJUiEFdlc3QgQmFieWxvbiwgTlkqFA1QWj0YFXSGQNQdYrBIGCUP70rUMAEaEW0gc3RldmVucyByb29maW5nIhFtIHN0ZXZlbnMgcm9vZmluZyoSUm9vZmluZyBjb250cmFjdG9y#ts=3'
+                target='_blank'
+                style={{
+                  height: 50,
+                  width: 50,
+                  padding: '5px',
+                  marginRight: '10px'
+                }}
+                bgColor='transparent'
+                className='bg-green-700 hover:text-white rounded-full hover:bg-gray-700'
               />
               <SocialIcon
                 url='https://www.angi.com/companylist/us/ny/medford/m-stevens-roofing-co-inc-reviews-484988.htm#reviews-section'
